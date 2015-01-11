758f91e1928d714514defa4f9767927803b1c88f
Eric Truong <etruong@excilys.com>
2015-01-11 22:08:39 +0100

    Basic behaviour

--- lib/src/main/java/android/widget/MaterialEditText.java
+++ lib/src/main/java/android/widget/MaterialEditText.java
@@ -19,32 +19,24 @@ public class MaterialEditText extends EditText {
     private float dimen_8dp;
     private float dimen_16dp;
     private float dimen_20dp;
+    private float dimen_2sp;
     private float dimen_12sp;
+    private float dimen_16sp;
 
-    private DashPathEffect pathEffect;
-    private float lineThickness;
+    private int highlightColor;
+
+    private DashPathEffect lineEffect;
+    private float lineWidth;
     private float lineHeight;
-    private int lineColor;
+    private float lineThickness;
     private Paint linePaint;
     private Path line;
 
     private CharSequence errorText;
-    private boolean drawError;
-    private int errorTextColor;
     private TextPaint errorTextPaint;
 
     private boolean floatingLabel;
-    private boolean drawLabel;
-    private int labelTextColor;
     private TextPaint labelTextPaint;
-
-    private int charCount;
-    private int maxCharCount;
-    private boolean drawCharCounter;
-    private int charCountTextColor;
-    private TextPaint charCountTextPaint;
-
-//    private boolean fullWidth;
 
     public MaterialEditText(Context context) {
         this(context, null);
@@ -72,16 +64,15 @@ public class MaterialEditText extends EditText {
         dimen_8dp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8, displayMetrics);
         dimen_16dp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16, displayMetrics);
         dimen_20dp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 20, displayMetrics);
+        dimen_2sp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 2, displayMetrics);
         dimen_12sp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 12, displayMetrics);
+        dimen_16sp = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16, displayMetrics);
 
-        lineColor = getCurrentHintTextColor();
-        errorTextColor = getCurrentHintTextColor();
-        labelTextColor = getCurrentHintTextColor();
+        highlightColor = getCurrentHintTextColor();
 
         if (!isEnabled()) {
-            pathEffect = new DashPathEffect(new float[]{ Math.round(dimen_1dp), Math.round(dimen_2dp) }, 0);
-            lineColor = getTextColors().getColorForState(new int[]{ -android.R.attr.state_enabled }, 0);
-            labelTextColor = getTextColors().getColorForState(new int[]{ -android.R.attr.state_enabled }, 0);
+            lineEffect = new DashPathEffect(new float[]{ Math.round(dimen_1dp), Math.round(dimen_2dp) }, 0);
+            highlightColor = getTextColors().getColorForState(new int[]{ -android.R.attr.state_enabled }, 0);
         }
         lineThickness = dimen_1dp;
         linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -92,24 +83,14 @@ public class MaterialEditText extends EditText {
         if (attrs != null) {
             TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.materialEditText, 0, 0);
             floatingLabel = ta.getBoolean(R.styleable.materialEditText_floatingLabel, false);
-            maxCharCount = ta.getInteger(R.styleable.materialEditText_maxCharacters, 0);
-//            fullWidth = ta.getBoolean(R.styleable.materialEditText_fullWidth, false);
         }
 
         // Override API padding
-//        if (fullWidth) {
-//            setPadding((int) dimen_16dp, (int) dimen_20dp, (int) dimen_16dp, (int) dimen_20dp);
-//        } else {
         setPadding(0, (int) dimen_16dp, 0, (int) dimen_16dp);
-        if (maxCharCount > 0) {
-            appendPadding(0, 0, 0, (int) (dimen_8dp + dimen_12sp));
-            charCountTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
-        }
         if (floatingLabel) {
             appendPadding(0, (int) (dimen_8dp + dimen_12sp), 0, 0);
             labelTextPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
         }
-//        }
         setIncludeFontPadding(false); // Remove text top/bottom padding)
         setBackground(null);  // Remove API background
         setTextSize(16);
@@ -121,33 +102,25 @@ public class MaterialEditText extends EditText {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        if (drawError && !TextUtils.isEmpty(errorText)) {
-            lineHeight = getHeight() - (2 * dimen_8dp + dimen_12sp);
-            errorTextPaint.setColor(errorTextColor);
-            errorTextPaint.setTextSize(dimen_12sp);
-            canvas.drawText(errorText, 0, errorText.length(), getScrollX(), getHeight() - dimen_8dp, errorTextPaint);
-        } else if (maxCharCount > 0) {
-            lineHeight = getHeight() - (2 * dimen_8dp + dimen_12sp);
-            if (drawCharCounter) {
-                charCountTextPaint.setColor(charCountTextColor);
-                charCountTextPaint.setTextSize(dimen_12sp);
-                charCountTextPaint.setTextAlign(Paint.Align.RIGHT);
-                String text = charCount + " / " + maxCharCount;
-                canvas.drawText(text, 0, text.length(), getScrollX() + getWidth(), getHeight() - dimen_8dp, charCountTextPaint);
-            }
-        } else {
-            lineHeight = getHeight() - dimen_8dp;
-        }
-        if (drawLabel && !TextUtils.isEmpty(getHint())) {
-            labelTextPaint.setColor(labelTextColor);
+        if (floatingLabel && !TextUtils.isEmpty(getHint())) {
+            labelTextPaint.setColor(highlightColor);
             labelTextPaint.setTextSize(dimen_12sp);
             canvas.drawText(getHint(), 0, getHint().length(), getScrollX(), dimen_16dp + dimen_12sp, labelTextPaint);
         }
 
+        if (!TextUtils.isEmpty(getError())) {
+            lineHeight = getHeight() - (2 * dimen_8dp + dimen_12sp);
+            errorTextPaint.setColor(highlightColor);
+            errorTextPaint.setTextSize(dimen_12sp);
+            canvas.drawText(getError(), 0, getError().length(), getScrollX(), getHeight() - dimen_8dp, errorTextPaint);
+        } else {
+            lineHeight = getHeight() - dimen_8dp;
+        }
+
         linePaint.setStyle(Paint.Style.STROKE);
-        linePaint.setColor(lineColor);
+        linePaint.setColor(highlightColor);
         linePaint.setStrokeWidth(lineThickness);
-        linePaint.setPathEffect(pathEffect);
+        linePaint.setPathEffect(lineEffect);
         line.reset();
         line.moveTo(getScrollX(), lineHeight);
         line.lineTo(getScrollX() + getWidth(), lineHeight);
@@ -159,28 +132,21 @@ public class MaterialEditText extends EditText {
         if (isEnabled()) {
             switch (event.getAction()) {
                 case MotionEvent.ACTION_DOWN:
-                    lineColor = obtainColorPrimary(getContext());
-                    labelTextColor = obtainColorPrimary(getContext());
+                    if (TextUtils.isEmpty(getError())) {
+                        highlightColor = obtainColorPrimary(getContext());
+                    }
                     lineThickness = dimen_2dp;
                     invalidate();
                     break;
                 case MotionEvent.ACTION_UP:
                 case MotionEvent.ACTION_CANCEL:
-                    if (isFocused()) {
-                        if (drawError || (drawCharCounter && charCount > maxCharCount)) {
-                            lineColor = getContext().getResources().getColor(R.color.material_red);
+                    if (!isFocused()) {
+                        if (TextUtils.isEmpty(getError())) {
+                            highlightColor = getCurrentHintTextColor();
                         }
-                    } else {
-                        if (drawError ) {
-                            lineColor = getContext().getResources().getColor(R.color.material_red);
-                            lineThickness = dimen_2dp;
-                        } else {
-                            lineColor = getCurrentHintTextColor();
-                            lineThickness = dimen_1dp;
-                        }
-                        labelTextColor = getCurrentHintTextColor();
+                        lineThickness = dimen_1dp;
+                        invalidate();
                     }
-                    invalidate();
                     break;
             }
         }
@@ -189,83 +155,44 @@ public class MaterialEditText extends EditText {
 
     @Override
     protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
-        if (maxCharCount > 0) {
-            if (focused) {
-                drawCharCounter = true;
-            } else {
-                drawCharCounter = false;
+        if (focused) {
+            if (TextUtils.isEmpty(getError())) {
+                highlightColor = obtainColorPrimary(getContext());
             }
-        }
-        if (!drawError) {
-            if (focused) {
-                if (drawCharCounter && charCount > maxCharCount) {
-                    lineColor = getContext().getResources().getColor(R.color.material_red);
-                } else {
-                    lineColor = obtainColorPrimary(getContext());
-                }
-                labelTextColor = obtainColorPrimary(getContext());
-                lineThickness = dimen_2dp;
-            } else {
-                lineColor = getCurrentHintTextColor();
-                labelTextColor = getCurrentHintTextColor();
-                lineThickness = dimen_1dp;
+            lineThickness = dimen_2dp;
+        } else {
+            if (TextUtils.isEmpty(getError())) {
+                highlightColor = getCurrentHintTextColor();
             }
+            lineThickness = dimen_1dp;
         }
         super.onFocusChanged(focused, direction, previouslyFocusedRect);
     }
 
     @Override
     public void setError(CharSequence error) {
-        if (error != null) {
-            errorText = error;
-            drawError = true;
-            appendPadding(0, 0, 0, (int) (dimen_8dp + dimen_12sp));
-            lineColor = getContext().getResources().getColor(R.color.material_red);
-            errorTextColor = getContext().getResources().getColor(R.color.material_red);
-            lineThickness = dimen_2dp;
-        } else {
-            errorText = null;
-            drawError = false;
+        errorText = error;
+        if (TextUtils.isEmpty(error)) {
             appendPadding(0, 0, 0, (int) -(dimen_8dp + dimen_12sp));
+        } else {
+            appendPadding(0, 0, 0, (int) (dimen_8dp + dimen_12sp));
+            highlightColor = getContext().getResources().getColor(R.color.material_red);
         }
+    }
+
+    @Override
+    public CharSequence getError() {
+        return errorText;
     }
 
     @Override
     public void setEnabled(boolean enabled) {
         if (enabled) {
-            pathEffect = null;
-//            currentColor = getCurrentHintTextColor();
+            lineEffect = null;
         } else {
-            pathEffect = new DashPathEffect(new float[]{ Math.round(dimen_1dp), Math.round(dimen_2dp) }, 0);
-//            currentColor = getTextColors().getColorForState(new int[]{ -android.R.attr.state_enabled }, 0);
+            lineEffect = new DashPathEffect(new float[]{ Math.round(dimen_1dp), Math.round(dimen_2dp) }, 0);
         }
         super.setEnabled(enabled);
-    }
-
-    @Override
-    protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
-        if (floatingLabel) {
-            if (text.length() > 0) {
-                drawLabel = true;
-            } else {
-                drawLabel = false;
-            }
-        }
-        if (maxCharCount > 0) {
-            charCount = text.length();
-            if (drawCharCounter) {
-                if (charCount > maxCharCount) {
-                    lineColor = getContext().getResources().getColor(R.color.material_red);
-                } else {
-                    lineColor = obtainColorPrimary(getContext());
-                }
-            }
-            if (charCount > maxCharCount) {
-                charCountTextColor = getContext().getResources().getColor(R.color.material_red);
-            } else {
-                charCountTextColor = getCurrentHintTextColor();
-            }
-        }
     }
 
     private void appendPadding(int left, int top, int right, int bottom) {
